0018b054377199acd814380d76e5459fab11d3f6
Adrian Dombeck <adrian.dombeck@rub.de>
2023-11-21 02:53:46 +0100

    Overwrite existing symlinks

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 CONFIG_FILE="$HOME/.config/transmission-nordvpn.conf"
 
 mkdir -p "$HOME/bin"
-ln -s "${SCRIPT_DIR}/bin/"* "$HOME/bin/"
-ln -s "${SCRIPT_DIR}/data/"*.desktop "$HOME/.local/share/applications/"
+ln -sf "${SCRIPT_DIR}/bin/"* "$HOME/bin/"
+ln -sf "${SCRIPT_DIR}/data/"*.desktop "$HOME/.local/share/applications/"
 
 # Create the config file
 touch "${CONFIG_FILE}"
